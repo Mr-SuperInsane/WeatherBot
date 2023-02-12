8770eab9495e5be40c77f1be1c4faf68125aacf8
Nao_kun <70059960+naokun-py@users.noreply.github.com>
2023-02-13 02:54:13 +0900

    Delete WeatherBot1.0 directory

--- WeatherBot1.0/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import os
-
-app = Flask(__name__)
-
-base_dir = os.path.dirname(__file__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
